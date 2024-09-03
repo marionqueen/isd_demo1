3113642e6b546ec0504e4f1e96954b51ac02f46a
Marion Queen Ramos <mramos3@academic.rrc.ca>
2024-09-02 20:43:18 -0500

    Updated library_item.py with _init_ and accessors

--- activities/activity_1/library_item/library_item.py
+++ activities/activity_1/library_item/library_item.py
@@ -1,5 +1,32 @@
 """"
 Description: A class to manage LibraryItem objects.
-Author: {Student Name}
-Date: {Date}
+Author: Marion Queen Ramos
+Date: 09.02.2024
 """
+
+from genre import Genre
+
+class LibraryItem:
+    def __init__(self, title: str, author: str, genre: Genre):
+        if not title:
+            raise ValueError("Title cannot be blank.")
+        if not author:
+            raise ValueError("Author cannot be blank.")
+        if not isinstance(genre, Genre):
+            raise ValueError("Invalid Genre.")
+        
+        self._title = title
+        self._author = author
+        self._genre = genre
+
+    @property
+    def title(self) -> str:
+        return self._title
+
+    @property
+    def author(self) -> str:
+        return self._author
+
+    @property
+    def genre(self) -> Genre:
+        return self._genre
